f53b049d85151fc5e1a7fdd8ebdff9f00bc029f7
jjosephllee <jjosephllee05@gmail.com>
2022-04-26 16:18:33 -0400

    copycopy

---
+++ Example-Project/include/op_control.hpp
@@ -0,0 +1,5 @@
+#include "api.h"
+void op_control();
+
+void useClaw();
+void useLift();
---
+++ Example-Project/include/robot_config.hpp
@@ -0,0 +1,5 @@
+#include "api.h"
+
+extern pros::Motor claw;
+extern pros::Motor lift;
+extern pros::Motor lift2;
